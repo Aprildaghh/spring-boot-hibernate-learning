8a4c2f51e46fb9ff8afe843e921297fda86c2af1
Abdullah Aslan <abdullah.aslan.se@gmail.com>
2023-08-12 00:37:20 +0300

    made progress on the section 8

--- section_8/src/main/java/com/april/section_8/controller/DemoController.java
+++ section_8/src/main/java/com/april/section_8/controller/DemoController.java
@@ -13,4 +13,16 @@ public class DemoController {
         return "home";
     }
 
+    @GetMapping("/leaders")
+    public String showLeaders()
+    {
+        return "leaders";
+    }
+
+    @GetMapping("/systems")
+    public String showSystems()
+    {
+        return "systems";
+    }
+
 }
