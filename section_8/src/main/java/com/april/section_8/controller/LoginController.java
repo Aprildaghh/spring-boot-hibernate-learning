0af2fab94097d26a0a022891303253e98e0625f6
Abdullah Aslan <abdullah.aslan.se@gmail.com>
2023-08-13 00:22:37 +0300

    added access denied page and restricted content based on roles on section 8

--- section_8/src/main/java/com/april/section_8/controller/LoginController.java
+++ section_8/src/main/java/com/april/section_8/controller/LoginController.java
@@ -13,4 +13,10 @@ public class LoginController {
         // return "plain-login";
     }
 
+    @GetMapping("/access-denied")
+    public String showAccessDenied()
+    {
+        return "access-denied";
+    }
+
 }
